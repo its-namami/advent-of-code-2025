ea9810763d0f27df562fa46e6188a9133c952fc7
its-namami <164639260+its-namami@users.noreply.github.com>
2025-12-02 19:10:18 +0100

    feat: change invalid ids to now include any repeating pattern
    Repeating patterns are, among others, 111, 1313, 8912389123 etc
    With this, part 2 is over

--- 2_gift_shop/main.rb
+++ 2_gift_shop/main.rb
@@ -1,22 +1,41 @@
 # frozen_string_literal: true
 
+require 'debug'
+
 input = File.read('./data/input.txt')
 
 ranges = input.split(',').inject([]) do |acc, range|
-  range = range.split('-').map { |number| number.to_i }
+  range = range.split('-').map(&:to_i)
 
   acc << Range.new(*range)
 end
 
-sum_invalid_ids = ranges.inject(0) do |sum, range|
-  range.each do |num|
-    string_number = num.to_s
-    num_start = string_number.slice!(0..(string_number.length / 2) - 1)
+def is_invalid(number)
+  (number.length / 2).times do |size|
+    checking = number[0..size]
+    is_invalid = true
 
-    sum += num if string_number == num_start
+    number.chars.each_slice(size + 1) do |slice|
+      unless slice.join == checking
+        is_invalid = false
+        break
+      end
+    end
+
+    return true if is_invalid
   end
 
-  sum
+  false
 end
 
-puts sum_invalid_ids
+invalid_ids = []
+
+ranges.each do |range|
+  range.each do |num|
+    invalid_ids << num if is_invalid(num.to_s)
+  end
+end
+
+puts 'All the invalid IDs showing repeating pattern is this:'
+puts invalid_ids
+puts "The sum of all invalid ids is #{invalid_ids.sum}"
