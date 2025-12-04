f647cb6a6adb20eddd4391012449709791460281
its-namami <164639260+its-namami@users.noreply.github.com>
2025-12-04 23:38:26 +0100

    feat: implement recursive forking of rolls
    As long as there is even one roll that is accessible
    i.e. has at most 3 other rolls around it
    it will take it, recursively until there are none more accessible left
    
    With this, the part 2 is completed

--- 4_printing_department/main.rb
+++ 4_printing_department/main.rb
@@ -5,7 +5,7 @@ require 'pp'
 
 ARR = File.foreach('./data/input.txt').with_object([]) do |line, arr|
   arr << line.chomp.each_char.to_a
-end
+end.freeze
 
 def neighbor_rolls(y, x)
   [
@@ -20,10 +20,26 @@ def neighbor_rolls(y, x)
   ].filter { |pair| pair.none?(&:negative?) && ARR.dig(*pair.reverse) == '@' }.size
 end
 
-qualified = ARR.each.with_object([]).with_index do |(arr, qualified), y|
-  arr.each.with_index do |elm, x|
-    qualified << [x, y] if elm == '@' && neighbor_rolls(y, x) < 4
+def qualified
+  ARR.each.with_object([]).with_index do |(arr, qualified_arr), y|
+    arr.each.with_index do |elm, x|
+      qualified_arr << [y, x] if elm == '@' && neighbor_rolls(y, x) < 4
+    end
   end
 end
 
-pp qualified.size
+def enxify(to_add)
+  to_add.each { |position| ARR[position.first][position.last] = 'X' }
+end
+
+def iterate_enxify
+  to_add = qualified
+
+  return ARR if to_add.empty?
+
+  enxify(to_add)
+
+  iterate_enxify
+end
+
+pp iterate_enxify.flatten.count('X')
