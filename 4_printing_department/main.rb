01a9be385deb9adc5dfbc69fa2242718644f49dc
its-namami <164639260+its-namami@users.noreply.github.com>
2025-12-04 20:13:24 +0100

    feat: calculate for all immediate neighbors and output sum
    With this, part one is completed

--- 4_printing_department/main.rb
+++ 4_printing_department/main.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 require 'debug'
+require 'pp'
 
-ARR = File.foreach('./data/test.txt').with_object([]) do |line, arr|
+ARR = File.foreach('./data/input.txt').with_object([]) do |line, arr|
   arr << line.chomp.each_char.to_a
 end
 
-def neighbor_papers(x, y)
+def neighbor_rolls(y, x)
   [
     [x - 1, y - 1],
     [x, y - 1],
@@ -16,7 +17,13 @@ def neighbor_papers(x, y)
     [x, y + 1],
     [x - 1, y + 1],
     [x - 1, y]
-  ].filter { |pair| pair.none?(&:negative?) && ARR.dig(*pair) == '@' }.size
+  ].filter { |pair| pair.none?(&:negative?) && ARR.dig(*pair.reverse) == '@' }.size
 end
 
-puts neighbor_papers(0, 0)
+qualified = ARR.each.with_object([]).with_index do |(arr, qualified), y|
+  arr.each.with_index do |elm, x|
+    qualified << [x, y] if elm == '@' && neighbor_rolls(y, x) < 4
+  end
+end
+
+pp qualified.size
